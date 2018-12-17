c580221e1a0252500e4d472ca98d1393a2cc6192
Matteo Muscas <thatman.j@gmail.com>
2018-12-17 13:53:51 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,17 @@
+class Appointment
+
+  @@all = [] 
+
+  def initialize(patient, doctor, date)
+    @patient = patient 
+    @doctor = doctor 
+    @date = date 
+    @@all << self 
+  end
+
+    def self.all 
+      @@all 
+    end
+
+end
+    
